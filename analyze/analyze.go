44a1dcee72abce85483c733c85ce51159923b4dc
Richard Gomez <32133502+rgmz@users.noreply.github.com>
2024-05-31 21:41:55 -0400

    chore: fix typo in format string

--- analyze/analyze.go
+++ analyze/analyze.go
@@ -138,13 +138,13 @@ func (a *Analyzer) AnalyzeOrg(ctx context.Context, org string, numberOfGoroutine
 
 				pkg, err := a.generatePackageInsights(ctx, tempDir, repo)
 				if err != nil {
-					errChan <- fmt.Errorf("failed to generate package insights for repo repoNameWithOwner: %w", err)
+					errChan <- fmt.Errorf("failed to generate package insights for repo %s: %w", repoNameWithOwner, err)
 					return
 				}
 
 				err = inventory.AddPackage(ctx, pkg, tempDir)
 				if err != nil {
-					errChan <- fmt.Errorf("failed to add package for repo repoNameWithOwner: %w", err)
+					errChan <- fmt.Errorf("failed to add package for repo %s: %w", repoNameWithOwner, err)
 					return
 				}
 				_ = bar.Add(1)
