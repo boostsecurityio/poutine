3e31467743a88b9f3a67375fd7c13095d5303e47
Alexis-Maurer Fortin <alexis@boostsecurity.io>
2024-07-16 17:59:41 -0400

    wip

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/boostsecurityio/poutine
 go 1.22.0
 
 require (
+	github.com/go-git/go-git/v5 v5.12.0
 	github.com/gofri/go-github-ratelimit v1.1.0
 	github.com/google/go-github/v59 v59.0.0
 	github.com/hashicorp/go-version v1.7.0
@@ -23,16 +24,25 @@ require (
 )
 
 require (
+	dario.cat/mergo v1.0.0 // indirect
+	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/OneOfOne/xxhash v1.2.8 // indirect
+	github.com/ProtonMail/go-crypto v1.0.0 // indirect
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/cloudflare/circl v1.3.7 // indirect
+	github.com/cyphar/filepath-securejoin v0.2.4 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
+	github.com/go-git/go-billy/v5 v5.5.0 // indirect
 	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
@@ -40,6 +50,8 @@ require (
 	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -47,6 +59,7 @@ require (
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pjbgf/sha1cd v0.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/prometheus/client_golang v1.19.1 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
@@ -56,14 +69,17 @@ require (
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
 	github.com/shurcooL/graphql v0.0.0-20230722043721-ed46e5a46466 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/skeema/knownhosts v1.2.2 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/tchap/go-patricia/v2 v2.3.1 // indirect
+	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/yashtewari/glob-intersection v0.2.0 // indirect
@@ -72,13 +88,18 @@ require (
 	go.opentelemetry.io/otel/sdk v1.26.0 // indirect
 	go.opentelemetry.io/otel/trace v1.26.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/term v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	sigs.k8s.io/yaml v1.4.0 // indirect
 )
--- opa/populate_build_platform_vuln_database_test.go
+++ opa/populate_build_platform_vuln_database_test.go
@@ -1,7 +1,179 @@
 package opa
 
-import "testing"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/boostsecurityio/poutine/providers/gitops"
+	"github.com/stretchr/testify/assert"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+type Severity struct {
+	Type  string `json:"type"`
+	Score string `json:"score"`
+}
+
+type CVEItem struct {
+	OsvId                   string     `json:"osv_id"`
+	Published               string     `json:"published"`
+	Aliases                 []string   `json:"aliases"`
+	Summary                 string     `json:"summary"`
+	Severity                []Severity `json:"severity"`
+	CweIds                  []string   `json:"cwe_ids"`
+	VulnerableVersions      []string   `json:"vulnerable_versions"`
+	VulnerableVersionRanges []string   `json:"vulnerable_version_ranges"`
+	VulnerableCommitSHAs    []string   `json:"vulnerable_commit_shas"`
+}
+
+type PlatformAdvisories map[string]map[string]CVEItem
+
+type CVEData struct {
+	CveMetadata struct {
+		ID            string `json:"cveId"`
+		DatePublished string `json:"datePublished"`
+	} `json:"cveMetadata"`
+	Containers struct {
+		CNA struct {
+			Affected []struct {
+				Vendor   string `json:"vendor"`
+				Product  string `json:"product"`
+				Versions []struct {
+					LessThanOrEqual string `json:"lessThanOrEqual"`
+					Version         string `json:"version"`
+					VersionType     string `json:"versionType"`
+				}
+			} `json:"affected"`
+			Descriptions []struct {
+				Value string `json:"value"`
+			}
+			ProblemTypes []struct {
+				Descriptions []struct {
+					CweId string `json:"cweId"`
+				}
+			} `json:"problemTypes"`
+			Metrics []struct {
+				CvssV31 struct {
+					VectorString string `json:"vectorString"`
+				} `json:"cvssV3_1"`
+			} `json:"metrics"`
+		} `json:"cna"`
+	} `json:"containers"`
+}
+
+func TransformCVEDataToAdvisories(cveData []CVEData) PlatformAdvisories {
+	advisories := PlatformAdvisories{}
+
+	for _, data := range cveData {
+		for _, affected := range data.Containers.CNA.Affected {
+			vendor := strings.ToLower(affected.Vendor)
+			if vendor != "github" && vendor != "gitlab" {
+				continue
+			}
+
+			cveItem := CVEItem{
+				OsvId:     data.CveMetadata.ID,
+				Published: data.CveMetadata.DatePublished,
+				Summary:   data.Containers.CNA.Descriptions[0].Value,
+				CweIds:    []string{},
+				Severity:  []Severity{},
+			}
+
+			// Populate CWE IDs
+			for _, problemType := range data.Containers.CNA.ProblemTypes {
+				for _, description := range problemType.Descriptions {
+					cveItem.CweIds = append(cveItem.CweIds, description.CweId)
+				}
+			}
+
+			// Populate Severity
+			for _, metric := range data.Containers.CNA.Metrics {
+				cveItem.Severity = append(cveItem.Severity, Severity{
+					Type:  "CVSS_V3", // Assuming all are CVSS V3 for simplicity
+					Score: metric.CvssV31.VectorString,
+				})
+			}
+
+			if _, ok := advisories[vendor]; !ok {
+				advisories[vendor] = make(map[string]CVEItem)
+			}
+			advisories[vendor][data.CveMetadata.ID] = cveItem
+		}
+	}
+
+	return advisories
+}
 
 func TestPopulateDatabase(t *testing.T) {
+	command := &gitops.ExecGitCommand{}
+	commands := []struct {
+		cmd  string
+		args []string
+	}{
+		{"git", []string{"init", "--quiet"}},
+		{"git", []string{"remote", "add", "origin", "https://github.com/CVEProject/cvelistV5"}},
+		{"git", []string{"config", "submodule.recurse", "false"}},
+		{"git", []string{"config", "core.sparseCheckout", "true"}},
+		{"git", []string{"config", "index.sparse", "true"}},
+		{"git", []string{"sparse-checkout", "init", "--sparse-index"}},
+		{"git", []string{"sparse-checkout", "set", "cves/2022", "cves/2023", "cves/2024"}},
+		{"git", []string{"fetch", "--quiet", "--no-tags", "--depth", "1", "--filter=blob:none", "origin", "main"}},
+		{"git", []string{"checkout", "--quiet", "-b", "target", "FETCH_HEAD"}},
+	}
 
+	tempDir, err := os.MkdirTemp("", "")
+	assert.NoError(t, err)
+
+	defer os.RemoveAll(tempDir)
+
+	for _, c := range commands {
+		if _, err := command.Run(context.TODO(), c.cmd, c.args, tempDir); err != nil {
+			assert.NoError(t, err)
+		}
+	}
+
+	cves := []CVEData{}
+
+	err = filepath.WalkDir(tempDir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() || !strings.HasSuffix(d.Name(), ".json") {
+			return nil
+		}
+
+		data, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		var cveData CVEData
+		if err := json.Unmarshal(data, &cveData); err != nil {
+			return err
+		}
+
+		if len(cveData.Containers.CNA.Affected) > 0 {
+			vendor := strings.ToLower(cveData.Containers.CNA.Affected[0].Vendor)
+			product := strings.ToLower(cveData.Containers.CNA.Affected[0].Product)
+			if vendor == "github" || vendor == "gitlab" {
+
+				if product == "github enterprise server" || product == "gitlab" {
+					cves = append(cves, cveData)
+					fmt.Println("Found matching file:", path)
+					// Process the file as needed
+				}
+
+			}
+		}
+
+		return nil
+	})
+
+	advisories := TransformCVEDataToAdvisories(cves)
+
+	assert.Len(t, advisories, 2)
 }
